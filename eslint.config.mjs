de2eed06a7946a41b83f48e5e842c37dcc7c5564
Christian Chang Thomaz <christianchangthomaz@gmail.com>
2025-03-26 16:03:01 -0300

    Project Structure - Eslint, Env, Fastify, Zod, Imports @

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      indent: ["tab"], // Usando tabulação para indentação
+      "max-len": ["error", { code: 80 }],
+    },
+  },
+]);
